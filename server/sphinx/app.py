e8a19cbd724b6e17002692b28a9b043556f0a41d
Djalma <djalmirinha@gmail.com>
2021-02-28 18:17:29 -0300

    Adiciona rota para pegar questão por id

--- server/sphinx/app.py
+++ server/sphinx/app.py
@@ -1,7 +1,43 @@
-from flask import Flask, jsonify
+from random import randint
+
+from flask import Flask, jsonify, request
+
+from .db.Database import Database
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Bem-viado caralho'
+
+@app.route('/questions/<id>')
+def get_question_by_id(id):
+    db = Database()
+
+    question_options = db.get_question_by_id(id)
+
+    response = {
+        'question': '',
+        'options': [],
+        'answer': ''
+    }
+
+    response['question'] = question_options['question'][0]
+ 
+    for o in question_options['options']:
+        response['options'].append(o[0])
+
+        if o[1] == 1:
+            response['answer'] = o[0]
+
+    return jsonify(response)
+
+@app.route('/categories')
+def get_categories():
+    db = Database()
+
+    categories = db.get_categories()
+
+    response = [{'title': c[1], 'id': c[0]} for c in categories]
+
+    return jsonify(response)
--- server/sphinx/db/Database.py
+++ server/sphinx/db/Database.py
@@ -7,6 +7,22 @@ class Database:
         self.conn = sqlite3.connect(DB_PATH)
         self.c = self.conn.cursor()
 
+    def get_question_by_id(self, id):
+        s = f'''SELECT text
+                FROM questions
+                WHERE id = ?
+            '''
+
+        s2 = f'''SELECT text, correct
+                 FROM options
+                 WHERE question_id = ?
+             '''
+
+        question = self.c.execute(s, [id]).fetchone()
+        options = self.c.execute(s2, [id]).fetchall()
+
+        return {'question': question, 'options': options}
+
     def get_categories(self):
         s = f'''SELECT id, title FROM categories'''
 
