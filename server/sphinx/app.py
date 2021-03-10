9c8f545b4cec1f315427a6ea90e10245ab189bf2
Djalma <djalmirinha@gmail.com>
2021-03-10 19:27:34 -0300

    Add Controller class to handle data processing

--- server/sphinx/app.py
+++ server/sphinx/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request
 
 from db.Database import Database
+from Controller import Controller
 
 app = Flask(__name__)
+
+controller = Controller()
 
 @app.route('/')
 def hello_world():
@@ -10,76 +13,25 @@ def hello_world():
 
 @app.route('/questions')
 def get_questions_by_category():
-    db = Database()
-
     category_id = request.args.get('category')
 
     # TODO: implement error treating
     if category_id is None:
         return 'Tá de sacanagem menor'
 
-    questions = db.get_questions_by_category(category_id)
+    questions = controller.get_questions_by_category(category_id)
 
-    response = []
-
-    for q in questions['questions']:
-        question = {
-            'text': '',
-            'options': []
-        }
-
-        question['text'] = q['text']
-        question['id'] = q['id']
-
-        for o in questions['options']:
-            option = dict()
-
-            if o['question_id'] == q['id']:
-                option['id'] = o['id']
-                option['text'] = o['text']
-
-                if o['correct'] == 1:
-                    question['answer_id'] = o['id']
-
-                question['options'].append(option)
-
-        response.append(question)
-
-    return jsonify(response)
+    return jsonify(questions)
 
 @app.route('/questions/<id>')
 def get_question_by_id(id):
-    db = Database()
-
-    question_options = db.get_question_by_id(id)
-    print(question_options)
-
-    response = {
-        'text': '',
-        'options': [],
-    }
-
-    response['id'] = question_options['question']['id']
-    response['text'] = question_options['question']['text']
-
-    for o in question_options['options']:
-        option = dict()
-
-        option['text'] = o['text']
-        option['id'] = o['id']
-
-        response['options'].append(option)
-
-        if o['correct'] == 1:
-            response['answer_id'] = o['id']
+    response = controller.get_question_by_id(id)
 
     return jsonify(response)
 
 @app.route('/categories')
 def get_categories():
-    db = Database()
-
-    categories = db.get_categories()
+    categories = controller.get_categories()
 
     return jsonify(categories)
 
