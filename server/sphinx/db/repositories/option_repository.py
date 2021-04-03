085331bd1431f0904ed5d2f2f381f248005def28
João Vitor Maia <maia.tostring@gmail.com>
2021-04-03 01:59:55 -0300

    Create option repository

---
+++ server/sphinx/db/repositories/option_repository.py
@@ -0,0 +1,15 @@
+
+from db.repositories.base_repository import BaseRepository
+
+
+class OptionRepository(BaseRepository):
+    def __init__(self):
+        self.table = "options"
+        super().__init__()
+
+    def get(self, option_id):
+        return self.db.query(
+            f"SELECT options.* FROM options WHERE id = :param LIMIT 1", param=option_id
+        )
+    
+
